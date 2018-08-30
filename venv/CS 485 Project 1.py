baf59ef59110fa3a9f2da4779ce448a1c6beba39
claythearc <cat0011@uah.edu>
2018-08-30 04:03:46 -0500

    added a list of numbers to check, and changed timing functions.

--- venv/CS 485 Project 1.py
+++ venv/CS 485 Project 1.py
@@ -2,22 +2,28 @@ import time
 import timeit
 
 def exhaustive(b: int,z: int,n: int):
+    print(f"Called with {b},{z},{n} ")
     """Brute Forces the answer to a discrete logarithm problem. """
     x = 0  # type: int
     while True: # loop x increasingly
-        if (b**x % n) == z: # if b^x is congruent to z mod n
+        if pow(b, x, n) == z: # if b^x is congruent to z mod n
             print(f"Answer: {x}")  # print the answer
             return x  # return the value
         x = x + 1  # increment x
 
 
 if __name__ == "__main__":  # run the code here.
-    b = int(input("Enter b: "))  # type: int
-    z = int(input("Enter z: "))  # type: int
-    n = int(input("Enter n: "))  # type: int
-
-    tic = time.process_time()
-    # measures execution time, over 1 run
-    print(timeit.timeit(f"exhaustive({b},{z},{n})", setup="from __main__ import exhaustive", number=1))
-    toc = time.process_time()
-    print(f"Process time: {toc-tic}")
+    numberlist = [
+        [97, 143, 97],
+        [2, 19, 3],
+        [3, 1452, 1999]
+    ]  # type: list(list(int))
+    for numset in numberlist:
+        tic = time.process_time()
+        walltic = time.time()
+        # measures execution time, over 1 run
+        exhaustive(*numset)
+        toc = time.process_time()
+        walltoc = time.time()
+        print(f"Process time: {toc-tic}")
+        print(f"Actual Time: {walltoc - walltic}")
